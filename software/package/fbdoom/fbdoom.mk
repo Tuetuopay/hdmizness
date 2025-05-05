5c328c52123b8acdef52593d6482abf259c37849
Tuetuopay <tuetuopay@me.com>
2025-05-06 01:20:43 +0200

    software: switch to FBDoom from my own fork

--- software/package/fbdoom/fbdoom.mk
+++ software/package/fbdoom/fbdoom.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-FBDOOM_VERSION = master
-FBDOOM_SITE = $(call github,maximevince,fbDOOM,$(FBDOOM_VERSION))
+FBDOOM_VERSION = b9aada4
+FBDOOM_SITE = $(call github,Tuetuopay,fbDOOM,$(FBDOOM_VERSION))
 FBDOOM_LICENSE = Unknown
 
 define FBDOOM_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) NOSDL=1 -C $(@D)/fbdoom
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) NOSDL=1 ARCH=arm -C $(@D)/fbdoom
 endef
 
 define FBDOOM_INSTALL_TARGET_CMDS
